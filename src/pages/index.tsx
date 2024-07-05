a01e625c1ff408cf8548baf1504ee1dfa9cebafe
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-07-05 20:41:32 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,29 +3,32 @@
 import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
 import { fetchTest, SET_TEST } from "@/reducers/test/test.reducer";
 import { RootState } from "@/store/store";
-import { useEffect } from "react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
-    const test = useAppSelector((state: RootState)=>state.test)
-    const dispatch = useAppDispatch()
+  const [mobile, setmobile] = useState(false)
 
-    const testHandler = ()=>{
-      dispatch(SET_TEST('Change text'))
-    }
+  const test = useAppSelector((state: RootState)=>state.test)
+  const dispatch = useAppDispatch()
 
-    useEffect(()=>{
-      dispatch(fetchTest())
-    },[])
+  const testHandler = ()=>{
+    setmobile(!mobile)
+  }
 
-    useEffect(()=>{
-      console.log(test.status)
-    }, [test])
-      
-    return (
-      <>
-      {test.test}
-      <button onClick={()=>testHandler()}>click</button>
-      </>
-    );
+  useEffect(()=>{
+    dispatch(fetchTest())
+  },[])
+
+  useEffect(()=>{
+    console.log(test.status)
+  }, [test])
+    
+  return (
+    <>
+    {mobile ? <p>{test.test+' mobile'}</p> : <p>{test.test+' desktop'}</p>}
+    <button onClick={()=>{testHandler()}}>click</button>
+    </>
+  );
 }
