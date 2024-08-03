5b3ee0c1d93e669990625d0f3fcf54ca6d43e031
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-08-03 19:03:42 +0300

    add header

--- src/view/main/main_view.tsx
+++ src/view/main/main_view.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer/Footer";
+import Header from "@/components/header/header";
 
 export default function MainView() {
     return (
         <>
-           <Footer/> 
+            <Header/>
+            <Footer/> 
         </>
     );
 };
