ac84384ae4b1bc970974c5c6399f37bbd92c9701
Vladislav Shamray <127450674+PanShamray@users.noreply.github.com>
2024-08-14 01:19:14 +0300

    created Buttons component, created JoinVolunteers component, add mobile adaptive design and English translation

--- src/language/languages.ts
+++ src/language/languages.ts
@@ -1,3 +1,5 @@
+import { title } from "process";
+
 export const languages = {
   UA: {
     //header
@@ -5,6 +7,13 @@ export const languages = {
     project: "Наші проєкти",
     support: "Підтримати",
     volunteers: "Волонтери",
+    //_____________________
+
+    //volunteers
+    main_page_title_volunteers: "Бажаєш долучитися до волонтерства?",
+    main_page_text_volunteers:
+      "Для цього треба зареєструватися на спеціальній волонтерській платформі, яка постійно долучає волонтерів до роботи та різних корисних зборів",
+    main_page_button_volunteers: "ДОЛУЧИТИСЬ",
     //_____________________
 
     //footer
@@ -20,10 +29,17 @@ export const languages = {
     support: "To Support",
     volunteers: "Volunteers",
     //_____________________
+
+    //volunteers
+    main_page_title_volunteers: "Do you want to volunteer?",
+    main_page_text_volunteers: "To do this, you need to register on a special volunteer platform, which constantly involves volunteers to work and various useful fees",
+    main_page_button_volunteers: "JOIN",
+    //_____________________
+    
     //footer
     menu: "Menu",
     contacts: "Contacts",
     privacy_policy: "Privacy policy",
-    offer: "Public offer"
+    offer: "Public offer",
   },
 };
--- src/view/main/main_view.tsx
+++ src/view/main/main_view.tsx
@@ -1,13 +1,16 @@
 import Baner from "@/components/baner/baner";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/header";
+import JoinVolunteers from "@/components/JoinVolunteers/JoinVolunteers";
 
 export default function MainView() {
     return (
-        <>
-            <Header/>
-            <Baner/>
-            <Footer/> 
-        </>
+      <>
+        <Header />
+        <Baner />
+
+        <JoinVolunteers />
+        <Footer />
+      </>
     );
 };
