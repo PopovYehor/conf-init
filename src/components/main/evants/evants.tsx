545bd2a0d5ac641469b634fc0b6166ef424d1a2e
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-08-18 16:20:38 +0300

    ref evant

--- src/components/main/evants/evants.tsx
+++ src/components/main/evants/evants.tsx
@@ -1,14 +1,22 @@
-import { useAppSelector } from "@/hooks/hooks"
+import { useAppDispatch, useAppSelector } from "@/hooks/hooks"
 import styles from "./evants.module.scss"
 import { languages } from "@/language/languages"
-import Link from "next/link"
-import { googleForm } from "@/constants/apiUrls/apiUrls"
+import { useEffect } from "react"
+import { fetchImage } from "@/reducers/image/image.reducer"
+import { IEventItem } from "@/interfaces/main/main.interface"
+import { EvantItem } from "./evantItem/evantItem"
 
 export default function Evants(){
 
     const languageSelected = useAppSelector((state)=>state.language.languageSelected)
-    const img = 'https://s3-alpha-sig.figma.com/img/122e/40f9/265a0ccf2786cb574d1180a13efc7f1d?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=h8HXWishVbW2S4jkk723Sq6ugLWL3tAUSArPpi2E4OOMakv-fmftiLTemEU7wVmfafQMUYupvwaLxZHXea2XsNXNG32TAFhCUPoHo7N3w6NhH8NJfUHNtrcPJyTHxVDvKuUZ~ygQIPd93vq39hs84zS6Y2AIBkBm5oDQrh5wuzJKSTIlB0MJX-kg0OESspoZ~LlAkcUbej3OfZ9WOxu5tv3vWzOQt-za0z6ZtZnteCcVPNSeZWXtZ5P2us17GPC6qVTzFQ9SXdQWndleDAC87YESaBjBKpwgA-FbypHSfQXHh~aQicattiLhs4jwIwKPfC5G3ahM19DD~pLUNthPJA__'
-    
+    const main = useAppSelector((state)=>state.main.main)
+    const dispatch = useAppDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchImage())
+        console.log(main)
+    },[])
+
     return(
         <>
         <article className={styles.evants_wrap}>
@@ -16,28 +24,21 @@ export default function Evants(){
                 <div className={styles.evants_header}>
                     <h2>{languageSelected === "UA"? languages.UA.evants : languages.EN.evants}</h2>
                 </div>
-                <section className={styles.evant_item_wrap}>
-                    <div className={styles.evant_item_img}>
-                        <img src={img}/>
-                    </div>
-                    <div className={styles.evant_item_container}>
-                        <div className={styles.evant_item_date}>
-                            <p>10 серпня 2024</p>
-                            <p> м. Київ, вул. Кучмин Яр, 1Б</p>
-                        </div>
-                        <div className={styles.evant_item_title}>
-                            <h3>Дружня зустріч сімей з дітьми з інвалідністю!</h3>
-                        </div>
-                        <div className={styles.evant_item_description}>
-                            <p>Це буде чудова можливість для дітей знайти нових друзів, провести весело час, а батькам – познайомитись, обмінятися досвідом, підтримкою та трішки відпочити в колі однодумців.
-                            Для участі, будь ласка, заповніть реєстраційну форму</p>
-                        </div>
-                        <div className={styles.evant_item_buttons}>
-                            <span className={styles.read_more}>{languageSelected === "UA"? languages.UA.read_more : languages.EN.read_more}</span>
-                            <Link className={styles.join} href={googleForm}>{languageSelected === "UA"? languages.UA.evant_join : languages.EN.evant_join}</Link>
-                        </div>
-                    </div>
-                </section>
+                {main.event.map((item: IEventItem)=>{
+                    return(
+                        <EvantItem 
+                        image ={item.image}
+                        dataEventUA={item.dataEventUA}
+                        dataEventEN={item.dataEventEN}
+                        adressEventUA={item.adressEventUA}
+                        adressEventEN={item.adressEventEN}
+                        descriptionEN={item.dataEventEN}
+                        descriptionUA={item.dataEventUA}
+                        titleEventUA={item.titleEventUA}
+                        titleEventEN={item.titleEventEN}
+                        />
+                    )
+                })}
             </div>
         </article>
         </>
--- src/view/main/main_view.tsx
+++ src/view/main/main_view.tsx
@@ -2,14 +2,24 @@ import Baner from "@/components/main/baner/baner";
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/header/header";
 import Evants from "@/components/main/evants/evants";
+import { fetchMain } from "@/reducers/main/main.reducer";
+import { useAppDispatch } from "@/hooks/hooks";
+import { useEffect } from "react";
 
 export default function MainView() {
+
+    const dispatch = useAppDispatch()
+
+    useEffect(()=>{
+        dispatch(fetchMain())
+    },[])
+    
     return (
         <>
             <Header/>
             <main>
             <Baner/>
-            <Evants/>
+            {/* <Evants/> */}
             </main>
             <Footer/> 
         </>
