e5d9a22abd3d121aecb2b54f9a50bc7062f73d40
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-09-02 14:45:55 +0300

    Update languages.ts

--- src/language/languages.ts
+++ src/language/languages.ts
@@ -12,7 +12,18 @@ export const languages: any = {
     evants: "Анонс подій",
     read_more: "Читати більше",
     evant_join: "Долучитись до події",
+    hide_text: 'Прихвати текст',
+    next: 'Далі',
+    previous: 'Назад',
+    without_event_description: "Хоча на даний час в нас немає запланованих заходів, однак ми вже працюємо над організацією нових! Приєднуйся до наших соціальних мереж та першими отримуйте інформацію про наступні події!",
+    without_event_header: "Наразі немає подій",
+    //________________________________
 
+    //How we help
+    how_help: "Як допомагаємо ?",
+    help_description_1: "Ми безпосередньо і тісно працюємо з людьми, які потребують допомоги, оскільки впевнені, що пряма комунікація та підтримка дозволить нам максимально ефективно визначити індивідуальні потреби кожної людини та/чи сім'ї та допомогти відповідно до наших можливостей.",
+    help_description_2: "Благодійна діяльність Конференції базується на повазі та любові до ближнього, починаючи з твердої відданості нашим спільним християнським цінностям.",
+    we_support:"Ми підтримуємо:",
     //Need support
     need_support_headline: "Якщо вам потрібна підтримка або допомога",
     need_support_text:
@@ -54,7 +65,17 @@ export const languages: any = {
     evants: "Announcement of events",
     read_more: "Read more",
     evant_join: "Join the event",
-
+    hide_text: 'Hide text',
+    next: 'Next',
+    previous: 'Previous',
+    without_event_description: "Although we do not currently have any events planned, we are already working on organizing new ones! Join our social networks and be the first to receive information about the next events!",
+    without_event_header: "There are currently no events",
+    //_________________________________
+    //How we help
+    how_help: "How we help ?",
+    help_description_1: "We work directly and closely with people who need help, because we believe that direct communication and support will allow us to determine the individual needs of each person and/or family as effectively as possible and help according to our capabilities.",
+    help_description_2: "The charitable activities of the Conference are based on respect and love for one's neighbor, starting with a firm commitment to our common Christian values.",
+    we_support:"We supports:",
     //Need support
     need_support_headline: "If you need support or help",
     need_support_text: "If you need help, please fill out the form here.",
