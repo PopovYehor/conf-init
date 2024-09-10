47583ef397e8d38b060766c10f7d961e5f9df742
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-09-10 15:14:29 +0300

    ref header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,85 +1,30 @@
-import Link from "next/link";
-import { IconsMain, IconsMenuClose, IconsMenuOpen } from "../icons/icons-main/icons-main";
-import styles from "./header.module.scss"
-import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
-import { useEffect, useState } from "react";
-import { checkMobile, checkMobileListener } from "@/hooks/mobile";
-import { FacebookIconsDefaultHeader, InstagramIconsDefaultHeader } from "../icons/icons-socials/icons-socials";
+import { useAppSelector } from "@/hooks/hooks";
 import { languages } from "@/language/languages";
 import { navigation } from "@/constants/navigations/navigations";
-import LanguageListComponent from "./language-list/language-list";
+import { HeaderDesctop } from "./header-desctop/header-desctop";
+import { HeaderMobile } from "./header-mobile/header-mobile";
+
+export interface IHeaderNavigation{
+    text: string
+    link: string
+}
+
+export interface IHeaderProps{
+    nav: IHeaderNavigation[]
+}
 
 export default function Header(){
 
     const langageSelected = useAppSelector((state)=>state.language.languageSelected)
     const isMobile = useAppSelector((state)=>state.mobile.mobile)
-    const [open, setOpen] = useState(false)
-
-    const nav = [
+    
+    const nav: IHeaderNavigation[] = [
         {text: languages[langageSelected].about, link: navigation.about},
         {text: languages[langageSelected].project, link: navigation.projects},
         {text: languages[langageSelected].support, link: navigation.support},
         {text: languages[langageSelected].volunteers, link: navigation.volunteers}
     ]
 
-    return (
-    <>
-    {!isMobile? 
-    <header className={styles.header}>
-        <div className={styles.header_wrap}>
-            <div className={styles.header_logo_wrap}>
-                <Link href={navigation.main}>
-                    <IconsMain/>
-                </Link>
-            </div>
-            <div className={styles.nav_wrap}>
-                <div className={styles.header_nav}>
-                    {nav.map((item)=>{
-                        return(
-                            <Link key={item.link} href={item.link}>{item.text}</Link>
-                        )
-                    })}
-                </div>
-                <div className={styles.header_language_wrap}>
-                    <LanguageListComponent/>
-                </div>
-            </div>
-        </div>
-    </header>
-    :
-    <header className={styles.header}>
-        <div className={styles.header_menu_wrap}>
-            <div className={styles.header_logo_wrap}>
-                <IconsMain/>
-            </div>
-            <input type="checkbox" id="menu"/>
-            <div className={styles.header_menu} >
-                <label htmlFor="menu" onClick={()=>setOpen(!open)}>
-                    {!open? <IconsMenuOpen/> : <IconsMenuClose/>}
-                </label>
-            </div>
-            <div className={styles.header_swap_menu}>
-                <div className={styles.empty_wrap}/>
-                <nav className={styles.header_nav}>
-                    {nav.map((item)=>{
-                        return(
-                            <Link key={item.link} href={item.link}>{item.text}</Link>
-                        )
-                    })}
-                </nav>
-                <div className={styles.header_swap_bottom}>
-                    <div className={styles.header_swap_socials}>
-                        <Link href={'/'}><InstagramIconsDefaultHeader/></Link>
-                        <Link href={'/'}><FacebookIconsDefaultHeader/></Link>
-                    </div>
-                    <div className={styles.header_language_wrap}>
-                        <LanguageListComponent/>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
-    }
-    </>
-    )
+    return (<>{!isMobile? <HeaderDesctop nav ={nav}/> : <HeaderMobile nav = {nav}/>}</>)
+
 }
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import { ReactNode, useEffect } from "react";
 import { Inter } from 'next/font/google'
-import Header from "@/components/header/header";
 import Footer from "@/components/Footer/Footer";
 import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
-import { CHANGE_LANGUAGE, SET_LANGUAGE_SELECTED } from "@/reducers/language/language.reducer";
+import { CHANGE_LANGUAGE, CHANGE_PAGE, SET_LANGUAGE_SELECTED } from "@/reducers/language/language.reducer";
 import { checkMobile, checkMobileListener } from "@/hooks/mobile";
 import { apiUrls } from "@/constants/apiUrls/apiUrls";
 import { fetchImage } from "@/reducers/image/image.reducer";
+import { locales } from "@/constants/languages/language";
+import Header from "@/components/Header/header";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -27,13 +28,20 @@ export default function Layout ({ children }: ILayout) {
     // check locales
     const replaceUrl: string = window.location.href.replace(apiUrls.urlPage, '')
     const langLocation: string = replaceUrl.slice(0,2)
-    if (langLocation == ''){
-      dispatch(SET_LANGUAGE_SELECTED('ua'))
-      dispatch(CHANGE_LANGUAGE('ua'))
-    }else{
-      dispatch(SET_LANGUAGE_SELECTED(langLocation)) 
-      dispatch(CHANGE_LANGUAGE(langLocation))
-    }
+    
+    locales.forEach((local)=>{
+      if (local == langLocation){
+        dispatch(SET_LANGUAGE_SELECTED(langLocation)) 
+        dispatch(CHANGE_LANGUAGE(langLocation))
+      }else{
+        dispatch(SET_LANGUAGE_SELECTED('ua'))
+        dispatch(CHANGE_LANGUAGE('ua'))
+      }
+    })
+
+    //check page
+    const page =  window.location.href.replace(apiUrls.urlPage+langLocation, '')
+    dispatch(CHANGE_PAGE(page))
 
     // mobile check
     if (mobileCheckStatus === null){
