dbc8b2e58d1065a7316028e06f3ef620f8034aa6
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-09-11 17:04:27 +0300

    Update header-desctop.tsx

--- src/components/Header/header-desctop/header-desctop.tsx
+++ src/components/Header/header-desctop/header-desctop.tsx
@@ -2,10 +2,10 @@ import { IconsMain } from "@/components/icons/icons-main/icons-main"
 import styles from "../header.module.scss"
 import Link from "next/link"
 import { navigation } from "@/constants/navigations/navigations"
-import { IHeaderNavigation, IHeaderProps } from "../header"
-import LanguageListComponent from "../language-list/language-list"
 import { useAppDispatch } from "@/hooks/hooks"
 import { CHANGE_PAGE } from "@/reducers/language/language.reducer"
+import { IHeaderNavigation, IHeaderProps } from "../header"
+import LanguageListComponent from "../language-list/language-list"
 
 export function HeaderDesctop({nav}: IHeaderProps){
 
