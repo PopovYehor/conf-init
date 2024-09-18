406db2c278316812b02be64e792a16df6bd34bb2
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-09-18 19:23:29 +0300

    Create ourProjectsItemsDefault.ts

---
+++ src/constants/ourProjectsItemsDefault/ourProjectsItemsDefault.ts
@@ -0,0 +1,11 @@
+import { IOurProject } from "@/interfaces/our-project/ourProject.interface";
+import { defaultImage } from "../mainItemsDefault/mainItemsDefault";
+
+export const ourProjectDefault: IOurProject = {
+    _id: "1",
+    titleOurProject: "Мета благодійних проєктів",
+    description: "– допомогти потребуючим, привернути увагу суспільства та громадян до проблем окремих груп населення, розвивати і культивувати в Україні традиції благодійності, милосердя, готовності прийти на допомогу ближньому.",
+    image: defaultImage,
+    language: "ua",
+    __v: 0
+}
