e0d926e8866ea93aec3e1dc30bec0d175aaf56ba
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-10-23 19:05:06 +0300

    add volunteers section component

---
+++ src/components/Volunteers/VolunteersDescription/VolunteersDescription.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import styles from './VolunteersDescription.module.scss'
+import { apiUrls, languageParameter } from '@/constants/apiUrls/apiUrls'
+import { useAppSelector } from '@/hooks/hooks'
+import axios from 'axios'
+import { volunteerSectionItemDefault } from '@/constants/volunteersItemsDeafault/volunteersItemsDeafault'
+import { IvolunteerSectionItemDefault } from '@/interfaces/volunteers/volunters.interface'
+import VolunteersDescriptionItem from './VolunteersDescription-Item/VolunteersDescription-Item'
+
+export default function VolunteersDescription(){
+
+    const languageSelected = useAppSelector((state)=>state.language.languageSelected)
+    const [data, setData] = useState<IvolunteerSectionItemDefault[]>([volunteerSectionItemDefault])
+
+    useEffect(()=>{
+        const fetchWorth = async ()=>{
+            try{
+                const response = await axios.get(apiUrls.volunteersSection+languageParameter+languageSelected)
+                setData(response.data)
+                console.log(response.data)
+            }catch{
+                setData([volunteerSectionItemDefault])
+                console.log("error")
+            }
+        }
+        fetchWorth()
+    },[])
+
+    return(
+        <>
+            <article className={styles.vulunteer_desc_wrap}>
+                <div className={styles.vulunteer_container}>
+                    {data.map((item)=>{
+                        return(
+                            <section className={styles.vulunteer_item_wrap} key={item._id}>
+                                <VolunteersDescriptionItem item={item}/>
+                            </section>
+                        )
+                    })}
+                </div>
+            </article>
+        </>
+    )
+}
