c73d4d19be27e2aa2e52ded679ae938de95088af
Yehor Popov <103197666+PopovYehor@users.noreply.github.com>
2024-11-14 16:27:17 +0200

    Update apiUrls.tsx

--- src/constants/apiUrls/apiUrls.tsx
+++ src/constants/apiUrls/apiUrls.tsx
@@ -1,6 +1,6 @@
 //url for get data
 /* const urlPage = 'http://localhost:3000/' */ // for local use
-const urlPage = 'conf-init.vercel.app' // for regular use
+const urlPage = 'https://conf-init.vercel.app/' // for regular use
 /* const url = 'http://localhost:5000/' */ // for local use
 const url = 'https://konf-backend.onrender.com/' // for regular use
 
